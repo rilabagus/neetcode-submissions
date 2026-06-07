32a2fd7f1f4f0dc5faa9e1c4eeb746b601cbc5d3
Spongegar <78991788+rilabagus@users.noreply.github.com>
2026-06-07 20:10:30 +0700

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        String res = "";
+        int max = 0;
+        Queue<String> q = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            q.add(s.substring(i, i == s.length() ? i : i+1));
+        }
+
+        System.out.println(q.isEmpty());
+        while (!q.isEmpty()) {
+            String ch = q.poll();
+            System.out.println(ch);
+
+            if(res.contains(ch)){
+                res = res.substring(res.indexOf(ch)+1);
+            }
+            res += ch;
+            max = Math.max(max, res.length());
+        }
+        return max;
+    }
+}

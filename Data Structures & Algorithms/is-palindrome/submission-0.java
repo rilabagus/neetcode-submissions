5e740f854f6d174002aebffd01dd7469b776cd77
Spongegar <78991788+rilabagus@users.noreply.github.com>
2026-06-09 03:57:22 +0700

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String cs = s.replaceAll("\\p{Punct}", "").replaceAll(" ","").toLowerCase();
+        char[] sl = cs.toCharArray();
+        char[] arr = new char[sl.length];
+        int idx = 0;
+        for(int i = sl.length - 1 ; i >=0 ; i--){
+            arr[idx] = sl[i];
+            idx++;
+        }
+        System.out.println("sl: " + Arrays.toString(sl));
+        System.out.println("arr: " + Arrays.toString(arr));
+        return Arrays.equals(sl,arr);
+    }
+}
